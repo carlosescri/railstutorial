0af2420347c8819d489744a60724ea7a66fd98f0
Carlos Escribano <carlosescri@gmail.com>
2015-10-06 20:07:41 +0200

    More tests
    Signed-off-by: Carlos Escribano <carlosescri@gmail.com>

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,7 +7,9 @@ Minitest::Reporters.use!
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
+
   include ApplicationHelper
+  include SessionsHelper
 
   # Add more helper methods to be used by all tests here...
 
@@ -15,4 +17,23 @@ class ActiveSupport::TestCase
   def is_logged_in?
     !session[:user_id].nil?
   end
+
+  # Logs in a test user
+  def log_in_as(user, options = {})
+    password = options[:password] || 'password'
+    remember_me = options[:remember_me] || '1'
+    if integration_test?
+      post login_path, session: { email: user.email, password: password,
+                                  remember_me: remember_me}
+    else
+      session[:user_id] = user.id
+    end
+  end
+
+  private
+
+    # Checks whether we are running an integration test or not.
+    def integration_test?
+      defined?(post_via_redirect)
+    end
 end
