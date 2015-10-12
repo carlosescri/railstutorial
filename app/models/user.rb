f79d40acf742c598df34858a6ec25aa3e4950d2e
Carlos Escribano <carlosescri@gmail.com>
2015-10-12 14:39:40 +0200

    Homepage feed for logged in users
    Signed-off-by: Carlos Escribano <carlosescri@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -73,6 +73,11 @@ class User < ActiveRecord::Base
     reset_sent_at < 2.hours.ago
   end
 
+  def feed
+    # The same as using "microposts" directly but we'll need this later
+    Micropost.where('user_id = ?', id)
+  end
+
   private
 
     # Converts email field to lowercase
