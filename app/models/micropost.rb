2a4a1430beef5dcef06977556b5164d578d9bae7
Carlos Escribano <carlosescri@gmail.com>
2015-10-12 17:33:04 +0200

    First steps on image uploading
    Signed-off-by: Carlos Escribano <carlosescri@gmail.com>

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -7,6 +7,8 @@ class Micropost < ActiveRecord::Base
   # which can then be evaluated with the call method.
   default_scope -> { order(created_at: :desc) }
 
+  mount_uploader :picture, PictureUploader
+
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
 end
