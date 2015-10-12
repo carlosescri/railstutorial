9c1c6cb19798a23d868ab91c9b40947d1c285053
Carlos Escribano <carlosescri@gmail.com>
2015-10-12 17:38:53 +0200

    Enhanced validations for images
    Signed-off-by: Carlos Escribano <carlosescri@gmail.com>

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -11,4 +11,15 @@ class Micropost < ActiveRecord::Base
 
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
+
+  # Custom validator, notice the use of "validate" instead of "validates"
+  validate :picture_size
+
+  private
+
+    def picture_size
+      if picture.size > 5.megabytes
+        errors.add(:picture, "should be less than 5MB")
+      end
+    end
 end
